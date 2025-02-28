c876c0594b75c564f43469ebab300ff09a74a459
ChicknTurtle <chickenturtle321@gmail.com>
2025-02-28 14:18:19 -0800

    Initial commit

---
+++ js/load-template.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function() {
+	const navContainer = document.getElementById("nav-container");
+	const footerContainer = document.getElementById("footer-container");
+
+	fetch("/templates/head.html")
+		.then(response => response.text())
+		.then(data => document.head.insertAdjacentHTML("beforeend", data))
+		.catch(error => console.error("Error loading head:", error));
+
+	fetch("/templates/nav.html")
+		.then(response => response.text())
+		.then(data => {
+			navContainer.innerHTML = data;
+			navContainer.style.width = "100%";
+			navContainer.style.height = "100%";
+
+			navContainer.querySelectorAll('script').forEach(script => {
+				const newScript = document.createElement('script');
+				newScript.textContent = script.textContent;
+				document.body.appendChild(newScript);
+			});
+
+			document.dispatchEvent(new Event('navLoaded'));
+		})
+		.catch(error => console.error("Error loading nav:", error));
+
+	fetch("/templates/footer.html")
+		.then(response => response.text())
+		.then(data => {
+			footerContainer.innerHTML = data;
+
+			footerContainer.querySelectorAll('script').forEach(script => {
+				const newScript = document.createElement('script');
+				newScript.textContent = script.textContent;
+				document.body.appendChild(newScript);
+			});
+
+			document.dispatchEvent(new Event('footerLoaded'));
+		})
+		.catch(error => console.error("Error loading footer:", error));
+});
---
+++ js/theme-toggle.js
@@ -0,0 +1,32 @@
+document.addEventListener('navLoaded', () => {
+
+    const themeToggle = document.getElementById('themeToggle');
+    const body = document.body;
+    const userPreference = localStorage.getItem('themePreference');
+
+    function applyTheme(isDark, skipTransition = false) {
+        if (skipTransition) body.style.transition = 'none';
+
+        body.classList.toggle('dark-theme', isDark);
+
+        themeToggle.src = isDark ? '/assets/images/dark-theme.png' : '/assets/images/light-theme.png';
+        themeToggle.setAttribute('theme', isDark ? 'dark' : 'light');
+
+        if (skipTransition) setTimeout(() => (body.style.transition = ''), 100);
+    }
+
+    function toggleDarkTheme() {
+        const isDark = body.classList.toggle('dark-theme');
+        localStorage.setItem('themePreference', isDark ? 'dark' : 'light');
+        applyTheme(isDark)
+    }
+
+    if (userPreference) {
+        applyTheme(userPreference, true);
+    } else {
+        const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        applyTheme(systemPrefersDark ? 'dark' : 'light', true);
+    }
+
+    themeToggle.addEventListener('click', toggleDarkTheme);
+});
